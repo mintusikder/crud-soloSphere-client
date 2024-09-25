170ca5b5f23cbf237d260dd4d459cb8014736823
Mintu Sikder <mintusikder15@gmail.com>
2024-09-25 21:27:44 +0600

    added propTypes

--- src/components/Slide.jsx
+++ src/components/Slide.jsx
@@ -1,6 +1,7 @@
+import PropTypes from "prop-types"; // ES6
 import { Link } from "react-router-dom";
 
-const Slide = ({image, text}) => {
+const Slide = ({ image, text }) => {
   return (
     <header>
       <div
@@ -12,7 +13,7 @@ const Slide = ({image, text}) => {
         <div className="flex items-center justify-center w-full h-full bg-gray-900/40">
           <div className="text-center">
             <h1 className="text-3xl font-semibold mb-4 text-white lg:text-4xl">
-          {text}
+              {text}
             </h1>
             <Link className="w-full px-5 py-2 mt-8 text-sm font-medium text-white capitalize transition-colors duration-300 transform bg-gray-400 rounded-md lg:w-auto hover:bg-gray-600 focus:outline-none focus:bg-gray-500">
               Post Job & Hire Expert
@@ -25,3 +26,7 @@ const Slide = ({image, text}) => {
 };
 
 export default Slide;
+Slide.propTypes = {
+    image: PropTypes.string.isRequired,
+    text: PropTypes.string.isRequired,
+};
